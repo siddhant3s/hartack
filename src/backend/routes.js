eb3bf4ec0c3d9514e248ee8a0c469e7201c4c7a3
Siddhant Sanyam <siddhant3s@gmail.com>
2017-02-12 02:07:23 -0800

    Uploaded files goes to harfiles

--- src/backend/routes.js
+++ src/backend/routes.js
@@ -1,12 +1,15 @@
 import { Router } from 'express';
 import multipart from 'connect-multiparty';
 
+const { copySync, ensureDirSync } = require('fs-extra');
+
 const router = new Router();
 const multipartMiddleware = multipart();
 
 router.post('/submitHar', multipartMiddleware, async (req, res) => {
   const { har } = req.files;
-  console.log('file', har.originalFilename, har.path, har.size);
+  ensureDirSync('./harfiles');
+  copySync(har.path, `./harfiles/${har.originalFilename}`);
   res.redirect('/');
 });
 
